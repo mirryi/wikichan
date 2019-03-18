b46592ca455de40b19d4d39a3da3dd2ff192141f
Dophin2009 <21zhaoe@gmail.com>
2019-03-17 23:30:33 -0400

    Selection of text works as intended

--- src/wikichan.ts
+++ src/wikichan.ts
@@ -1,29 +1,120 @@
-import { onMouseMove } from "./hover";
+window.addEventListener("mouseover", getWordUnderCursor);
 
-window.addEventListener("mousemove", onMouseMove);
+class TextSource {
+    private _leftNode: Node;
+    private _rightNode: Node;
+    private _text: string;
+    private _offset: number;
 
-$(function () {
-    $("p span").each(function () {
-        var $this = $(this);
-        var text = $this.text();
-        $this.replaceWith(text);
-    });
+    constructor(node: Node, offset: number) {
+        this._leftNode = node;
+        this._rightNode = node;
+        this._text = node.textContent;
+        this._offset = offset;
+    }
 
-    $("p").each(function () {
-        var $this = $(this);
-        var newText = $this.text().replace(/([\s])([^\s]+)/g, "$1<span>$2</span>");
-        newText = newText.replace(/^([^\s]+)/g, "<span>$1</span>");
-        $this.empty().append(newText);
-    });
+    phrase(before: number, after: number): string {
+        let i: number = this.offset;
+        let begin: number;
+        let end: number;
 
-    replace("h1")
-});
+        for (let k = 0; k < before + 1; k++) {
+            while (i > 0) {
+                i--;
+                if (this.text[i] === " ") break;
+            }
+            if (i === 0) {
+                this.joinBefore();
+            }
+        }
+        begin = i;
 
-function replace(tag: string) {
-    $(tag).each(function () {
-        var $this = $(this);
-        var newText = $this.text().replace(/([\s])([^\s]+)/g, "$1<span>$2</span>");
-        newText = newText.replace(/^([^\s]+)/g, "<span>$1</span>");
-        $this.empty().append(newText);
-    });
+        i = this.offset;
+        for (let k = 0; k < after + 1; k++) {
+            while (i < this.text.length) {
+                i++;
+                if (this.text[i] === " ") break;
+            }
+            if (i === this.text.length) {
+                this.joinAfter();
+            }
+        }
+        end = i;
+        return this.text.substring(begin, end).trim();
+    }
+
+    joinAfter(): void {
+        this.rightNode = getNextNode(this.rightNode);
+        this.addAfter(this.rightNode.textContent);
+    }
+
+    joinBefore(): void {
+        this.leftNode = getPreviousNode(this.leftNode);
+        this.addBefore(this.leftNode.textContent);
+    }
+
+    addAfter(text: string): void {
+        this.text += text;
+    }
+
+    addBefore(text: string): void {
+        this.text = text + this.text;
+        this.offset += text.length;
+    }
+
+    get text(): string {
+        return this._text;
+    }
+
+    set text(value: string) {
+        this._text = value;
+    }
+
+    get offset(): number {
+        return this._offset;
+    }
+
+    set offset(value: number) {
+        this._offset = value;
+    }
+
+    get leftNode(): Node {
+        return this._leftNode;
+    }
+
+    set leftNode(value: Node) {
+        this._leftNode = value;
+    }
+
+    get rightNode(): Node {
+        return this._rightNode;
+    }
+
+    set rightNode(value: Node) {
+        this._rightNode = value;
+    }
+}
+
+function getNextNode(node: Node): Node {
+    return node.nextSibling === null ? node.parentNode.nextSibling : node.nextSibling;
+}
+
+function getPreviousNode(node: Node): Node {
+    return node.previousSibling === null ? node.parentNode.previousSibling : node.previousSibling;
+}
+
+function getWordUnderCursor(event: MouseEvent) {
+    const range: CaretPosition = document.caretPositionFromPoint(event.clientX, event.clientY);
+    const textNode: HTMLObjectElement = <HTMLObjectElement>range.offsetNode;
+    const offset: number = range.offset;
+
+    // console.log(getPreviousNode(textNode).textContent + data + getNextNode(textNode).textContent);
+
+    const source = new TextSource(textNode, offset);
+    console.log(source.phrase(5, 5));
+
+    // console.log(source.text);
+    // source.joinAfter();
+    // source.joinBefore();
+    // console.log(source.text);
 }
