a75cf54bdebae0d7671056f7f515bfc0de41feaa
Dophin2009 <21zhaoe@gmail.com>
2019-03-18 23:40:56 -0400

    Implement multiple text selection and querying

--- src/wikichan.ts
+++ src/wikichan.ts
@@ -2,11 +2,12 @@ import { WikiApi } from "./api/wikiapi";
 import { getSourceUnderCursor } from "./doc-bind/mouse-bind";
 import { TextSource } from "./doc-bind/text-source";
 import { ResponseDisplay } from "./doc-bind/display";
+import { WikiApiResponse } from "./api/response";
 
 declare global {
-	interface Window {
-		wikiframe: ResponseDisplay;
-	}
+    interface Window {
+        //wikiframe: ResponseDisplay;
+    }
 }
 
 class Wikichan {
@@ -18,15 +19,25 @@ class Wikichan {
 
     prepare() {
         window.addEventListener('mousedown', this.onMouseOver.bind(this));
-        window.wikiframe = new ResponseDisplay(0, 0, 0, 0);
+        //window.wikiframe = new ResponseDisplay(0, 0, 0, 0);
     }
 
     onMouseOver(e: MouseEvent) {
         const source: TextSource = getSourceUnderCursor(e);
-        this.wikic.fetchExtract(source.phrase(0, 0))
-            .then(function (res) {
-                console.log(res);
-            });
+
+        let responses: WikiApiResponse[] = [];
+        for (let before = 0; before < 4; before++) {
+            for (let after = 0; after < 4; after++) {
+                this.wikic.fetchExtract(source.phrase(before, after))
+                    .then(function (res: WikiApiResponse) {
+                        console.log(res);
+                        responses.push(res);
+                    }, function (e: Error) {
+                        console.log(e.stack);
+                    });
+            }
+        }
+
     }
 
 }
