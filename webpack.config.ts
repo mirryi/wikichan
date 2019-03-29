f9dd3c24e7947857289c77129c79c3bc05c975c1
Dophin2009 <21zhaoe@gmail.com>
2019-03-28 20:13:13 -0400

    Fix Webpack entry point

--- webpack.config.ts
+++ webpack.config.ts
@@ -7,7 +7,7 @@ const outdir = path.resolve(__dirname, 'ext');
 module.exports = {
     entry: {
         wikichan: ['./src/ts/content/wikichan.ts', './src/scss/injected.scss'],
-        frame: ['./src/ts/content/frame/frame.ts', './src/scss/frame.scss']
+        frame: ['./src/ts/frame/frame.ts', './src/scss/frame.scss']
     },
     module: {
         rules: [
