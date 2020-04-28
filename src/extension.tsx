6037c53089aa0245e39727b9f4bbb377c6b30266
Eric Zhao <21zhaoe@protonmail.com>
2020-04-28 09:40:26 -0400

    feat: Hover with ctrl instead of click with alt to show

--- src/extension.tsx
+++ src/extension.tsx
@@ -5,7 +5,7 @@ import { Float } from "./components/float";
 import { RootComponent } from "./components/root";
 import { ProviderMerge } from "./provider";
 import { WikipediaLanguage, WikipediaProvider } from "./providers/wikipedia";
-import { ExpandMode, getTextSourceFromPoint } from "./selector";
+import { ExpandMode, getTextSourceFromPoint, TextSource } from "./selector";
 
 if (self === top) {
   const floatRef = React.createRef<Float>();
@@ -25,16 +25,41 @@ if (self === top) {
   ReactDOM.render(ext, tmp);
   document.body.appendChild(tmp.childNodes[0]);
 
-  fromEvent(window, "click").subscribe((e: Event) => {
+  let lastSource: TextSource | null = null;
+  const highlightDiv: HTMLDivElement = document.createElement("div");
+  document.body.appendChild(highlightDiv);
+  highlightDiv.style.background = "yellow";
+  highlightDiv.style.position = "absolute";
+  highlightDiv.style.zIndex = "-1";
+
+  let interval = 0;
+
+  fromEvent(window, "mousemove").subscribe((e: Event) => {
     const me = e as MouseEvent;
-    if (me.altKey) {
+    if (me.ctrlKey) {
       const x = me.clientX;
       const y = me.clientY;
+
+      if (lastSource) {
+        const lastRect = lastSource.range.getBoundingClientRect();
+        if (
+          x >= lastRect.left &&
+          x <= lastRect.left + lastRect.width &&
+          y >= lastRect.top &&
+          y <= lastRect.top + lastRect.height
+        ) {
+          return;
+        }
+      }
+
+      clearInterval(interval);
+      floatRef.current?.close();
 
       let ts = getTextSourceFromPoint(x, y, [1, 0], [1, 0]);
       if (ts === null) {
         return;
       }
+      const initialSource = ts;
 
       const queries: string[] = [ts.text()];
 
@@ -68,10 +93,30 @@ if (self === top) {
           }
         }
       }
+
       rootRef.current?.searchProviders(queries);
 
-      floatRef.current?.open(x, y);
-    } else {
+      const newRect = initialSource.range.getBoundingClientRect();
+      highlightDiv.style.left = `${newRect.left}px`;
+      highlightDiv.style.top = `${newRect.top}px`;
+      highlightDiv.style.width = `${newRect.width}px`;
+      highlightDiv.style.height = `${newRect.height}px`;
+
+      interval = window.setInterval(() => {
+        const rootState = rootRef.current?.state;
+        if (rootState && rootState.items.length > 0) {
+          floatRef.current?.open(x, y);
+          clearInterval(interval);
+        }
+      }, 100);
+
+      lastSource = initialSource;
+    }
+  });
+
+  fromEvent(window, "click").subscribe((e: Event): void => {
+    const me = e as MouseEvent;
+    if (!me.ctrlKey) {
       floatRef.current?.close();
     }
   });
