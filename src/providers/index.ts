276590be6139843ee4a76d14a232161a25d460bb
Eric Zhao <21zhaoe@protonmail.com>
2020-05-07 13:19:03 -0400

    refactor: Wrapper class for cached providers

--- src/providers/index.ts
+++ src/providers/index.ts
@@ -1,5 +1,8 @@
 import { ReactNode } from "react";
-import { merge, Observable } from "rxjs";
+import { merge, Observable, from, of } from "rxjs";
+import { mergeMap, tap } from "rxjs/operators";
+
+import { Cache } from "@common/cache";
 
 export interface Item {
   title: string;
@@ -36,4 +39,54 @@ export class ProviderMerge {
     const searches = this.providers.map((pr) => pr.search(queries));
     return merge(...searches);
   }
+}
+
+export abstract class CachedProvider<T extends Item> implements Provider<T> {
+  cache: Cache;
+  provider: Provider<T>;
+
+  name(): string {
+    return this.provider.name();
+  }
+
+  constructor(provider: Provider<T>, cache: Cache) {
+    this.provider = provider;
+    this.cache = cache;
+  }
+
+  search(queries: string[]): Observable<T> {
+    const promises = queries.map(async (q) => {
+      return this.cache.get(this.key(q)).then(
+        (v) => {
+          if (!v) {
+            return q;
+          }
+          return this.deserializeItem(v);
+        },
+        () => {
+          return q;
+        },
+      );
+    });
+
+    return merge(...promises.map((p) => from(p))).pipe(
+      mergeMap((v) => {
+        if (typeof v !== "string") {
+          return of(v);
+        }
+
+        return this.provider.search([v as string]).pipe(
+          tap((item) => {
+            const serialized = this.serializeItem(item);
+            this.cache.set(this.key(item.searchTerm), serialized);
+          }),
+        );
+      }),
+    );
+  }
+
+  abstract serializeItem(item: T): string;
+  abstract deserializeItem(str: string): T;
+
+  abstract key(k: string): string;
 }
--- src/providers/wikipedia/cached.ts
+++ src/providers/wikipedia/cached.ts
@@ -1,65 +1,30 @@
-import { Observable, from, of, merge } from "rxjs";
-import { tap, mergeMap } from "rxjs/operators";
-
 import { Cache } from "@common/cache";
+import { CachedProvider } from "@providers";
 
 import { WikipediaProvider, WikipediaLanguage, WikipediaItem } from "./index";
 
-export class CachedWikipediaProvider extends WikipediaProvider {
-  cache: Cache;
-
+export class CachedWikipediaProvider extends CachedProvider<WikipediaItem> {
   constructor(language: WikipediaLanguage, cache: Cache) {
-    super(language);
+    super(new WikipediaProvider(language), cache);
     this.cache = cache;
   }
 
-  search(queries: string[]): Observable<WikipediaItem> {
-    const promises = queries.map(async (q) => {
-      return this.cache.get(this.key(q)).then(
-        (v) => {
-          if (!v) {
-            return q;
-          }
-          return this.deserializeItem(v);
-        },
-        () => {
-          return q;
-        },
-      );
-    });
-
-    return merge(...promises.map((p) => from(p))).pipe(
-      mergeMap((v) => {
-        if (typeof v !== "string") {
-          return of(v);
-        }
-
-        return super.search([v as string]).pipe(
-          tap((item) => {
-            const serialized = this.serializeItem(item);
-            this.cache.set(this.key(item.searchTerm), serialized);
-          }),
-        );
-      }),
-    );
-  }
-
-  private serializeItem(item: WikipediaItem): string {
+  serializeItem(item: WikipediaItem): string {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const toSerialize = item as any;
     toSerialize.tags = [...item.tags.entries()];
     return JSON.stringify(toSerialize);
   }
 
-  private deserializeItem(str: string): WikipediaItem {
+  deserializeItem(str: string): WikipediaItem {
     const parsed = JSON.parse(str);
     const item = parsed as WikipediaItem;
     item.tags = new Map(parsed["tags"]);
-    item.provider = this;
+    item.provider = this.provider as WikipediaProvider;
     return item;
   }
 
-  private key(k: string): string {
+  key(k: string): string {
     return "wikipedia_" + k;
   }
 }
