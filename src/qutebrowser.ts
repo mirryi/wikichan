87e8347733dd88b868ecdefd0c1bfb5088ae9b6f
Eric Zhao <21zhaoe@protonmail.com>
2020-12-08 11:40:45 -0500

    fix: Use GMCache for qutebrowser cache items

--- src/qutebrowser.ts
+++ src/qutebrowser.ts
@@ -7,18 +7,21 @@ import { Provider, ProviderMerge } from "@providers";
 import { WikipediaLanguage, CachedWikipediaProvider } from "@providers/wikipedia";
 import { CachedOwlBotProvider } from "@providers/owlbot";
 
-import ServerCache from "./qutebrowser/storage";
+// import ServerCache from "./qutebrowser/storage";
+import GMCache from "./userscript/storage";
 
 (function (): void {
   if (self !== top) {
     return;
   }
 
-  const cache = new ServerCache("http://127.0.0.1:5000");
+  const gmCache = new GMCache("");
+  // const globalCache = new ServerCache("http://127.0.0.1:5000");
+
   const defaultCacheDuration = 24 * 60 * 60;
 
   const providers: Provider[] = [
-    new CachedWikipediaProvider(WikipediaLanguage.EN, cache, defaultCacheDuration),
+    new CachedWikipediaProvider(WikipediaLanguage.EN, gmCache, defaultCacheDuration),
   ];
   const providerMerge = new ProviderMerge(providers);
 
@@ -27,7 +30,7 @@ import ServerCache from "./qutebrowser/storage";
     console.warn("OwlBot API token not provided; cannot query OwlBot");
   } else {
     providers.push(
-      new CachedOwlBotProvider(owlbotToken as string, cache, defaultCacheDuration),
+      new CachedOwlBotProvider(owlbotToken as string, gmCache, defaultCacheDuration),
     );
   }
 
