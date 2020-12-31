b8f6b96fa3789e46c6985f34beb4e27790099a97
Eric Zhao <21zhaoe@protonmail.com>
2020-12-31 18:46:42 -0500

    build: Cleanup eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,14 +11,17 @@ module.exports = {
       version: "detect",
     },
   },
+  env: {
+    browser: true,
+    node: false,
+  },
+  globals: { process: "readonly" },
   plugins: ["@typescript-eslint"],
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/eslint-recommended",
     "plugin:@typescript-eslint/recommended",
     "prettier/@typescript-eslint",
-    "plugin:prettier/recommended",
-    "plugin:react/recommended",
     "plugin:no-unsanitized/DOM",
   ],
   rules: {
