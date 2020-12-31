4c248e7b0c13d7d49b476e30ea7f21ca991aa416
Eric Zhao <21zhaoe@protonmail.com>
2020-12-31 18:49:12 -0500

    Revert "build: Cleanup eslint config"
    This reverts commit b8f6b96fa3789e46c6985f34beb4e27790099a97.

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,17 +11,14 @@ module.exports = {
       version: "detect",
     },
   },
-  env: {
-    browser: true,
-    node: false,
-  },
-  globals: { process: "readonly" },
   plugins: ["@typescript-eslint"],
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/eslint-recommended",
     "plugin:@typescript-eslint/recommended",
     "prettier/@typescript-eslint",
+    "plugin:prettier/recommended",
+    "plugin:react/recommended",
     "plugin:no-unsanitized/DOM",
   ],
   rules: {
