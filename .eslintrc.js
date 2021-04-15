2ac35fb2e504e13be486b63370673c1790824700
Eric Zhao <21zhaoe@protonmail.com>
2021-04-15 17:01:43 -0400

    style: Rework eslint and prettier configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,36 +1,38 @@
 module.exports = {
-  root: true,
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
+    root: true,
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        ecmaVersion: 12,
+        sourceType: "module",
+        ecmaFeatures: {
+            jsx: true,
+        },
     },
-  },
-  settings: {
-    react: {
-      version: "detect",
+    extends: [
+        "plugin:@typescript-eslint/recommended",
+        "plugin:react/recommended",
+        "prettier/@typescript-eslint",
+        "plugin:no-unsanitized/DOM",
+    ],
+    rules: {
+        "no-use-before-define": "off",
+        "@typescript-eslint/no-use-before-define": [
+            "error",
+            { functions: false, classes: false, variables: true },
+        ],
+        "@typescript-eslint/explicit-function-return-type": ["error"],
+        "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
     },
-  },
-  env: {
-    browser: true,
-    node: false,
-  },
-  globals: {
-    process: "readonly",
-  },
-  plugins: ["@typescript-eslint"],
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/eslint-recommended",
-    "plugin:@typescript-eslint/recommended",
-    "prettier/@typescript-eslint",
-    "plugin:prettier/recommended",
-    "plugin:react/recommended",
-    "plugin:no-unsanitized/DOM",
-  ],
-  rules: {
-    "prettier/prettier": 0,
-    "@typescript-eslint/no-use-before-define": "off",
-    "@typescript-eslint/triple-slash-reference": "off",
-  },
+    settings: {
+        react: {
+            version: "detect",
+        },
+    },
+    env: {
+        browser: true,
+        node: false,
+    },
+    globals: {
+        process: "readonly",
+    },
 };
