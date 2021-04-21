6fefd335b27f5b000846bf445350a5be81372615
Eric Zhao <21zhaoe@protonmail.com>
2021-04-21 16:14:00 -0400

    feat: More util types

--- src/util/index.ts
+++ src/util/index.ts
@@ -1,3 +1,9 @@
 export type Immutable<T> = {
     readonly [K in keyof T]: Immutable<T[K]>;
 };
+
+export type DistributiveOmit<T, K extends keyof any> = T extends any ? Omit<T, K> : never;
+
+export type DeepPartial<T> = {
+    [P in keyof T]?: T[P] extends object ? DeepPartial<T[P]> : T[P];
+};
