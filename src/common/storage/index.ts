02435ff44228bca103b1c3c264f933533960dca8
Eric Zhao <21zhaoe@protonmail.com>
2021-05-09 18:29:59 -0400

    feat: Add Map-based memory PlatformStorage impl

--- src/common/storage/index.ts
+++ src/common/storage/index.ts
@@ -1,4 +1,5 @@
 export * from "./central";
+export * from "./memory";
 export * from "./platform";
 export * from "./temporary";
 export * from "./validated";
--- src/util/index.ts
+++ src/util/index.ts
@@ -9,10 +9,14 @@ export type DeepPartial<T> = {
     [P in keyof T]?: T[P] extends Record<string, unknown> ? DeepPartial<T[P]> : T[P];
 };
 
-export type Entries<T> = { [K in keyof T]: [K, T[K]] }[keyof T][];
+export type Entries<T> = {
+    [P in keyof T]: [P, T[P]];
+}[keyof T][];
 export namespace Entries {
-    // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
+    /* eslint-disable @typescript-eslint/consistent-type-assertions, @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-return */
     export const from = <T>(o: T): Entries<T> => Object.entries(o) as Entries<T>;
+    export const into = <T>(entries: Entries<T>): T => Object.fromEntries(entries) as any;
+    /* eslint-enable @typescript-eslint/consistent-type-assertions, @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-return */
 }
 
 export type StringLiteral<T> = T extends string ? (string extends T ? never : T) : never;
