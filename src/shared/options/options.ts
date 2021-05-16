c494620f5241bc4f54eec0f5ca0702dedfef821e
Eric Zhao <21zhaoe@protonmail.com>
2021-05-16 17:21:57 -0400

    feat: Version options for easy migration

--- src/shared/options/options.ts
+++ src/shared/options/options.ts
@@ -6,13 +6,17 @@ export { ValidationSchema };
 import { BackOptions } from "./back-options";
 import { FrontOptions } from "./front-options";
 
-export interface Options {
+export interface OptionsV1 {
+    version: "v1";
     back: BackOptions;
     front: FrontOptions;
 }
 
+export type Options = OptionsV1;
+
 export namespace Options {
     export const Schema: ValidationSchema<Options> = s.object({
+        version: s.defaulted(s.literal("v1"), "v1"),
         back: s.defaulted(BackOptions.Schema, BackOptions.Schema.create({})),
         front: s.defaulted(FrontOptions.Schema, FrontOptions.Schema.create({})),
     });
