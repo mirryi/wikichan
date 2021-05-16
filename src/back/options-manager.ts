ba574b7b86ff8b98f224e2a3ed56d446b690614f
Eric Zhao <21zhaoe@protonmail.com>
2021-05-16 15:45:31 -0400

    fix: Use rxjs-observe to reactively save options

--- src/back/options-manager.ts
+++ src/back/options-manager.ts
@@ -1,4 +1,5 @@
-import { observable, autorun } from "mobx";
+import { switchMap } from "rxjs/operators";
+import { observe } from "rxjs-observe";
 
 import { Options } from "@shared/options";
 import { DeepPartial } from "@util";
@@ -8,17 +9,24 @@ import { OptionsHandle } from "./back-storage";
 
 const OPTIONS_KEY = "root";
 
-export class OptionsManager {
+interface OptionsProxy {
     options: Options;
+}
+export class OptionsManager {
+    private proxy: OptionsProxy;
 
     private constructor(options: Options, private storageHandle: OptionsHandle) {
-        this.options = observable(options);
-        autorun(async () => await this.save());
+        const { observables, proxy } = observe({ options });
+        this.proxy = proxy;
+
+        observables.options.pipe(switchMap(() => this.save())).subscribe();
     }
 
     static async load(storageHandle: OptionsHandle): Promise<OptionsManager> {
         // TODO: less duplicate code
         const loaded = await storageHandle.get([OPTIONS_KEY]);
+
+        // TODO: Catch validation error
         const options = Options.Schema.create(loaded[OPTIONS_KEY] || {});
 
         return new OptionsManager(options, storageHandle);
@@ -26,17 +34,19 @@ export class OptionsManager {
 
     // TODO: reexport the newOptions type from message module.
     change(newOptions: DeepPartial<Options>): void {
-        // TODO: This is wrong; need to ensure that the new `options` is still
-        // being observed.
-        this.options = deepmerge(this.options, newOptions);
+        this.proxy.options = deepmerge(this.proxy.options, newOptions);
     }
 
     async load(): Promise<void> {
         const loaded = await this.storageHandle.get([OPTIONS_KEY]);
-        this.options = Options.Schema.create(loaded[OPTIONS_KEY]);
+        this.proxy.options = Options.Schema.create(loaded[OPTIONS_KEY]);
     }
 
     async save(): Promise<void> {
-        await this.storageHandle.set({ [OPTIONS_KEY]: this.options });
+        await this.storageHandle.set({ [OPTIONS_KEY]: this.proxy.options });
+    }
+
+    get options(): Options {
+        return this.proxy.options;
     }
 }
